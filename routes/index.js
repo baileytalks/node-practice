2d9831c326de5c9c46cf19db3f5f49a8361a8052
Bailey Kursar <bkursar@gmail.com>
2017-05-16 21:07:19 +0100

    Committing changes although does not work in local host

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var models = require('../models');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  models.Tasks.all().then(function(taskList) {
+    res.render('index', {tasks: taskList});
+  });
+});
+
+router.post('/add-task', function(req, res) {
+  models.Tasks
+        .build({
+            title: req.body.taskName,
+            completed: false})
+        .save()
+        .then(function() {
+          models.Tasks.findAll({}).then(function(taskList) {
+                res.render('index', {tasks: taskList});
+            });
+        });
 });
 
 module.exports = router;
